d209220a8d313bac970a0a47e9e66de8c2ecaa6f
ALBA <ALBA@Alba>
2015-03-12 13:44:23 +0100

    P2: 12/03/2015  13:45

--- ControlVersionesED/src/Paciente.java
+++ ControlVersionesED/src/Paciente.java
@@ -5,7 +5,18 @@ public class Paciente {
 	private int edad;
 	
 	public Paciente() {
+		edad=20;
+		nombre="pepe";
+	}
+	
+	public int compareTo(Object o){
+		Paciente p = (Paciente) o;
 		
+		if(nombre.compareTo(p.nombre) < 0) return -1;
+		else if (nombre.compareTo(p.nombre) > 0) return 1;
+		else if(edad < p.edad) return -1;
+		else if(edad > p.edad) return 1;
+		else return 0;
 	}
 	
 }
