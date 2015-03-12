2adf9c591113ec9b124bb2a24e75a04a96d65252
Hikarito <Hikaru@Hikarito-PC>
2015-03-12 13:03:15 +0100

    base

---
+++ ControlVersionesED/src/Documento.java
@@ -0,0 +1,26 @@
+
+public class Documento {
+
+	/**
+	 * Lorem fistrum está la cosa muy malar está la cosa 
+	 * muy malar te va a hasé pupitaa a peich a gramenawer. 
+	 * Está la cosa muy malar tiene musho peligro amatomaa 
+	 * hasta luego Lucas a peich papaar papaar jarl a 
+	 * gramenawer llevame al sircoo. De la pradera ese 
+	 * hombree ese hombree va usté muy cargadoo papaar 
+	 * papaar está la cosa muy malar diodeno a wan. Al 
+	 * ataquerl hasta luego Lucas por la gloria de mi madre 
+	 * torpedo diodenoo condemor. Amatomaa a peich a wan 
+	 * hasta luego Lucas te va a hasé pupitaa hasta luego 
+	 * Lucas qué dise usteer no puedor de la pradera torpedo 
+	 * a gramenawer. Qué dise usteer va usté muy cargadoo a 
+	 * wan por la gloria de mi madre diodeno fistro. Hasta 
+	 * luego Lucas pecador no puedor no te digo trigo por 
+	 * no llamarte Rodrigor ahorarr caballo blanco caballo 
+	 * negroorl. Caballo blanco caballo negroorl pupita me 
+	 * cago en tus muelas al ataquerl de la pradera llevame 
+	 * al sircoo ese hombree. Apetecan quietooor jarl torpedo. 
+	 */
+	
+	
+}
