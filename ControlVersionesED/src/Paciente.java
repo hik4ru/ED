9d729d00474cf44f7b9a3c0befc15971f5e4d56f
ALBA <ALBA@Alba>
2015-04-16 12:31:23 +0200

    asdadgsfhdh

--- ControlVersionesED/src/Paciente.java
+++ ControlVersionesED/src/Paciente.java
@@ -9,6 +9,14 @@ public class Paciente {
 		nombre="pepe";
 	}
 
-	asdf
+	public int compareTo(Object o){
+		Paciente p = (Paciente) o;
+		
+		if(nombre.compareTo(p.nombre) < 0) return -1;
+		else if (nombre.compareTo(p.nombre) > 0) return 1;
+		else if(edad < p.edad) return -1;
+		else if(edad > p.edad) return 1;
+		else return 0;
+	}
 	
 }
