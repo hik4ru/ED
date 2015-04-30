3bc243f3ec609d3e736ce46fc74d3717e6deb6aa
Hikarito <Hikaru@Hikarito-PC>
2015-04-30 12:59:19 +0200

    push 4

--- ControlVersionesED/src/Paciente.java
+++ ControlVersionesED/src/Paciente.java
@@ -8,7 +8,18 @@ public class Paciente {
 		nombre="pepe";
 	}
 	
+	
+	
 	public boolean equals(Paciente p) {
 		return this.nombre==p.nombre;
+	}
+	public int compareTo(Object o){
+		Paciente p = (Paciente) o;
+		
+		if(nombre.compareTo(p.nombre) < 0) return -1;
+		else if (nombre.compareTo(p.nombre) > 0) return 1;
+		else if(edad < p.edad) return -1;
+		else if(edad > p.edad) return 1;
+		else return 0;
 	}
 }
